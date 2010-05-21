f22927c3cb3cdb1c27c018cbf71345bcd80150d1
dante <d.didomenico@channelweb.it>
2010-05-21 16:32:59 +0000

    books plugin/module bugfixes

--- config/sql/bedita_schema.sql
+++ config/sql/bedita_schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS `books`;
 
 CREATE TABLE books (
   id INTEGER UNSIGNED NOT NULL,
-  isbn CHAR(13) NULL COMMENT 'Internationa standard book number, can be NULL',
+  isbn CHAR(13) NULL COMMENT 'International standard book number, can be NULL',
   year INT(4) NULL DEFAULT NULL COMMENT 'publication or edition year, can be NULL',
   series VARCHAR(255) NULL COMMENT 'book serie, can be NULL',
   location VARCHAR(255) NULL COMMENT 'book location (for library), can be NULL',
